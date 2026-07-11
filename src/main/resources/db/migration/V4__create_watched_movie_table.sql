8d0dc1c153e6e23172c7a46141553f99bd5082fc
RPFacco <arpfacco@gmail.com>
2026-07-11 14:42:56 -0300

    feat: create database schema via flyway migrations

---
+++ src/main/resources/db/migration/V4__create_watched_movie_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE watched_movie (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    rating DOUBLE PRECISION,
+    review TEXT,
+    watched_at DATE NOT NULL,
+    favorite BOOLEAN NOT NULL DEFAULT FALSE,
+    user_id UUID NOT NULL REFERENCES users(id),
+);
