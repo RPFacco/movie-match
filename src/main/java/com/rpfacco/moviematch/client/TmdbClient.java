dd925ff54e445ffea11fcda9958dc28046dae736
RPFacco <arpfacco@gmail.com>
2026-07-13 18:04:24 -0300

    feat: add discoverMovies method on TmdbClient

--- src/main/java/com/rpfacco/moviematch/client/TmdbClient.java
+++ src/main/java/com/rpfacco/moviematch/client/TmdbClient.java
@@ -1,4 +1,34 @@
 package com.rpfacco.moviematch.client;
 
+import com.rpfacco.moviematch.client.dto.TmdbDiscoverResponseDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class TmdbClient {
+
+    private final RestClient tmdbRestClient;
+
+    public TmdbClient(RestClient tmdbRestClient) {
+        this.tmdbRestClient = tmdbRestClient;
+    }
+
+    public TmdbDiscoverResponseDTO discoverMovies(List<Long> genreIds, Double minScore) {
+        String genres = genreIds.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        return tmdbRestClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/discover/movie")
+                        .queryParam("with_genres", genres)
+                        .queryParam("vote_average.gte", minScore)
+                        .build())
+                .retrieve()
+                .body(TmdbDiscoverResponseDTO.class);
+    }
+
 }
