f052e8b44522fa35a3de6100363263184ef67bbf
RPFacco <arpfacco@gmail.com>
2026-07-13 17:48:00 -0300

    feat: change to tmdb access token and add RestClient bean

--- src/main/java/com/rpfacco/moviematch/config/TmdbConfig.java
+++ src/main/java/com/rpfacco/moviematch/config/TmdbConfig.java
@@ -1,15 +1,25 @@
 package com.rpfacco.moviematch.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class TmdbConfig {
 
-    @Value("${tmdb.api.key}")
-    private String apiKey;
+    @Value("${tmdb.access-token}")
+    private String accessToken;
 
     @Value("${tmdb.base-url}")
     private String baseUrl;
+
+    @Bean
+    public RestClient tmdbRestClient() {
+        return RestClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader("Authorization", "Bearer " + accessToken)
+                .build();
+    }
 
 }
