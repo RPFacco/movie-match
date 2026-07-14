cee0fc467233516e7b903bdb02dc373f649666c0
RPFacco <arpfacco@gmail.com>
2026-07-14 16:05:16 -0300

    feat: hash user password on creation using PasswordEncoder

--- src/main/java/com/rpfacco/moviematch/service/UserService.java
+++ src/main/java/com/rpfacco/moviematch/service/UserService.java
@@ -4,22 +4,25 @@ import com.rpfacco.moviematch.domain.user.User;
 import com.rpfacco.moviematch.domain.user.dto.UserRequestDTO;
 import com.rpfacco.moviematch.domain.user.dto.UserResponseDTO;
 import com.rpfacco.moviematch.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public UserResponseDTO createUser(UserRequestDTO data) {
         User newUser = new User();
         newUser.setName(data.name());
         newUser.setEmail(data.email());
-        newUser.setPassword(data.password());
+        newUser.setPassword(passwordEncoder.encode(data.password()));
 
         User savedUser = repository.save(newUser);
 
