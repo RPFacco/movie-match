5ec01523fa45ec051afc029266f96ee8ae162cd8
RPFacco <arpfacco@gmail.com>
2026-07-14 18:26:27 -0300

    fix: add global security requirement

--- src/main/java/com/rpfacco/moviematch/config/OpenApiConfig.java
+++ src/main/java/com/rpfacco/moviematch/config/OpenApiConfig.java
@@ -2,6 +2,9 @@ package com.rpfacco.moviematch.config;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -12,4 +15,9 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
 }
