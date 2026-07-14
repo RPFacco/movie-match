57533706ab99c3039ea447ff313454049e22d254
RPFacco <arpfacco@gmail.com>
2026-07-14 18:04:16 -0300

    feat: update WatchedMovieController to use @AuthenticationPrincipal in post and get methods

--- src/main/java/com/rpfacco/moviematch/controller/WatchedMovieController.java
+++ src/main/java/com/rpfacco/moviematch/controller/WatchedMovieController.java
@@ -1,10 +1,12 @@
 package com.rpfacco.moviematch.controller;
 
+import com.rpfacco.moviematch.domain.user.User;
 import com.rpfacco.moviematch.domain.watchedmovie.dto.WatchedMovieRequestDTO;
 import com.rpfacco.moviematch.domain.watchedmovie.dto.WatchedMovieResponseDTO;
 import com.rpfacco.moviematch.service.WatchedMovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,13 +25,13 @@ public class WatchedMovieController {
     @PostMapping
     public ResponseEntity<WatchedMovieResponseDTO> create(
             @RequestBody WatchedMovieRequestDTO data,
-            @RequestParam UUID userId) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(watchedMovieService.createWatchedMovie(data, userId));
+            @AuthenticationPrincipal User user) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(watchedMovieService.createWatchedMovie(data, user.getId()));
     }
 
     @GetMapping
-    public ResponseEntity<List<WatchedMovieResponseDTO>> findAll(@RequestParam UUID userId) {
-        return ResponseEntity.ok(watchedMovieService.findAllWatchedMoviesByUser(userId));
+    public ResponseEntity<List<WatchedMovieResponseDTO>> findAll(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(watchedMovieService.findAllWatchedMoviesByUser(user.getId()));
     }
 
     @PutMapping("/{id}")
